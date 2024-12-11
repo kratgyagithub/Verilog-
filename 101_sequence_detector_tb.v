b661b329c59f9358f59445d1fd84c9ccf8dcb2ba
Kratgya Shrivastava <129892910+kratgyagithub@users.noreply.github.com>
2024-12-11 13:45:50 +0530

    Update 101_sequence_detector_tb.v

--- 101_sequence_detector_tb.v
+++ 101_sequence_detector_tb.v
@@ -2,25 +2,25 @@ module seq_101_tb;
 reg in , R, clk;
 wire out;
 
-seq_101 uut( .in(in),.R(R),.clk(clk),.out(out));
+  seq_101 uut( .in(in),.R(R),.clk(clk),.out(out)); //instantiation
 
-initial begin
+initial begin // initialising clock of 10us time period
   clk = 0;
   forever #5 clk =~clk;
 end
-initial begin
+initial begin  // reset initialization
   R = 1;
   #15;
   R = 0;
   #100;
 end
-initial begin
+initial begin // stimulus for the dut
   #15 in = 0; #10 in = 1; #10 in = 0; #10 in = 1;
   #10 in = 1; #10 in = 0; #10 in = 1; #10 in = 1;
   #10;
   $finish;
 end
 initial begin
-  $monitor("%t|| IN = %b || OUT = %b || R = %b",$time, in, out,R);
+  $monitor("%t|| IN = %b || OUT = %b || R = %b",$time, in, out,R); // monitor initialization
 end
 endmodule
